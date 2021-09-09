a9f5448a0f352daf0bc0ffc50d1fa97a21c54dd1
shim0494 <shim0494470@gmail.com>
2021-09-09 20:46:58 +0900

    오류 수정(바텀네비 삭제됨), 홈화면 기본화면 작성

---
+++ app/src/main/java/com/example/fakenews_factchecker/kakao_search.kt
@@ -0,0 +1,18 @@
+package com.example.fakenews_factchecker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class kakao_search : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_kakao_search, container, false)
+    }
+}
