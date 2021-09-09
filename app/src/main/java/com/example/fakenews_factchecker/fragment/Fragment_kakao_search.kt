4e7f70134a4fb4eeb29572479ad8ea81347e0cff
jjh2x <jjh3706@gmail.com>
2021-09-10 00:53:32 +0900

    준호 - 개발전 초기.

--- app/src/main/java/com/example/fakenews_factchecker/fragment/Fragment_kakao_search.kt
+++ app/src/main/java/com/example/fakenews_factchecker/fragment/Fragment_kakao_search.kt
@@ -1,12 +1,13 @@
-package com.example.fakenews_factchecker
+package com.example.fakenews_factchecker.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fakenews_factchecker.R
 
-class kakao_search : Fragment() {
+class fragment_kakao_search : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
